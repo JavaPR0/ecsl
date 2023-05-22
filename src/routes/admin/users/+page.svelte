6b9a01620fb77d7e56d75f89ac083ec8aa4c488f
Javadobe <meshachmacwright10@gmail.com>
2023-05-22 22:27:41 +0000

    User Role Fixed

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -50,9 +50,9 @@
 									name="role"
 									class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								>
-									<option>Admin</option>
-									<option>Observer</option>
-									<option>Agent</option>
+									<option value="admin">Admin</option>
+									<option value="observer">Observer</option>
+									<option value="agent">Agent</option>
 								</select>
 							</div>
 						</div>
